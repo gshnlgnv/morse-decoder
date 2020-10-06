fed6a4441c26aa08a2e2e93824fcceccc057d321
gshnlgnv <gshnlgnv@gmail.com>
2020-10-06 12:08:30 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -35,10 +35,49 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let codedExpr = expr.match(/.{1,10}/g);
+    let morseArr = [];
+    let finalText = "";
+
+    codedExpr.map((elem) => {
+        morseArr.push(elem.match(/.{1,2}/g));
+    });
+
+    const decodingToMorseFunc = (morseArr) => {
+        for (let i = 0; i < morseArr.length; i++) {
+            for (let j = 0; j < morseArr.length; j++) {
+                if (morseArr[i][j] === "10") {
+                    morseArr[i][j] = ".";
+                } else if (morseArr[i][j] === "11") {
+                    morseArr[i][j] = "-";
+                } else if (morseArr[i][j] === "00") {
+                    morseArr[i].splice([j], 1);
+                    decodingToMorseFunc(morseArr);
+                }
+            }
+        }
+        return morseArr;
+    };
+
+    decodingToMorseFunc(morseArr);
+
+    const finalArr = morseArr.map(elem => {
+        return elem.join("");
+    });
+
+    for (let i = 0; i < finalArr.length; i++) {
+        for (let key in MORSE_TABLE) {
+            if (key === finalArr[i]) {
+                finalText += MORSE_TABLE[key];
+            }
+        }
+    }
+
+    return finalText;
 }
 
 module.exports = {
